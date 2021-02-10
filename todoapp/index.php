1027c7e0d10259796c24d61b5034c38e2bf2ecfb
khakan <khakan@khakan.com>
2021-02-10 13:03:13 +0000

    third commit

--- todoapp/index.php
+++ todoapp/index.php
@@ -5,7 +5,7 @@
         <title> THIS IS TEST PAGE  </title>
     </head>
     <body>
-        <h1>Ma page web</h1>
+        <h1> TEST PAGE</h1>
         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
     </body>
 </html>
