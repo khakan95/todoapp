987385e27dce62ab0c2180120e34e578ee9069cb
khakan <khakan@khakan.com>
2021-02-10 13:15:43 +0000

    five commit

--- todoapp/index.php
+++ todoapp/index.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title> THIS IS statging page  </title>
+        <title> THIS IS production page  </title>
     </head>
     <body>
-        <h1> STAGING PAGE</h1>
+        <h1> PRODUCTION PAGE</h1>
         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
     </body>
 </html>
