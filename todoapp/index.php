a2d6c0c71df667aa819f0575333a7f40ea296867
khakan <khakan@khakan.com>
2021-02-15 11:16:31 +0000

    prod1

--- todoapp/index.php
+++ todoapp/index.php
@@ -5,7 +5,7 @@
         <title> THIS IS production page  </title>
     </head>
     <body>
-        <h1> PRODUCTION PAGE</h1>
+        <h1> PRODUCTION1111 </h1>
         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
     </body>
 </html>
